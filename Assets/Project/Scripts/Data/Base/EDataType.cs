fe65107668c6c0232d1daabebf4dc8a032f7d79e
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-06-24 18:22:30 +0900

    feat : 정적 데이터를 담을 SO 스크립트 작성

--- Assets/Project/Scripts/Data/Base/EDataType.cs
+++ Assets/Project/Scripts/Data/Base/EDataType.cs
@@ -1,7 +1,13 @@
-/// <summary>
+﻿/// <summary>
 /// 데이터 타입을 정의하는 열거형입니다.
 /// </summary>
 public enum EDataType
 {
     None = 0,
+    Enemy = 1,
+    Item = 2,
+    GrabTool = 3,
+    Oxygen = 4,
+    Thruster = 5,
+    Radar = 6,
 }
