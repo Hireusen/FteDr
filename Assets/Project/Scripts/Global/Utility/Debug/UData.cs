035b9da2fd3487832119ce381a22d1c22e51c856
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-06-27 16:32:11 +0900

    feat : 데이터베이스 매니저 퍼사드 패턴 적용
    SO를 편하게 가져오기 위한 유틸리티 클래스 UData.cs 작성

---
+++ Assets/Project/Scripts/Global/Utility/Debug/UData.cs
@@ -0,0 +1,73 @@
+﻿/// <summary>
+/// UDatabaseManager의 진입점 역할을 하는 유틸리티 클래스입니다.
+/// </summary>
+public static class UData
+{
+    public static CDatabaseManager Address => CDatabaseManager.Ins;
+
+    /// <summary>
+    /// 문자열 ID로 SO를 반환받습니다.
+    /// </summary>
+    /// <typeparam name="T">가져올 SO 타입</typeparam>
+    /// <param name="id">정적 SO ID</param>
+    /// <returns></returns>
+    public static T Get<T>(string id) where T : ABaseSO
+        => Address.Get<T>(id);
+
+    /// <summary>
+    /// 문자열 ID로 ABaseSO를 반환받습니다.
+    /// 구체적인 타입을 반환받으려면 제네릭을 사용해야 합니다.
+    /// </summary>
+    /// <param name="id">정적 SO ID</param>
+    /// <returns></returns>
+    public static ABaseSO Get(string id)
+    => UData.Get<ABaseSO>(id);
+
+    /// <summary>
+    /// 문자열 ID로 수집품 SO를 반환받습니다.
+    /// </summary>
+    /// <param name="id">수집품 ID</param>
+    /// <returns></returns>
+    public static CCollectibleSO Collectible(string id)
+        => Address.Get<CCollectibleSO>(id);
+
+    /// <summary>
+    /// 문자열 ID로 적 SO를 반환받습니다.
+    /// </summary>
+    /// <param name="id">적 ID</param>
+    /// <returns></returns>
+    public static CEnemySO Enemy(string id)
+        => Address.Get<CEnemySO>(id);
+
+    /// <summary>
+    /// 문자열 ID로 잡기 도구 SO를 반환받습니다.
+    /// </summary>
+    /// <param name="id">잡기 도구 ID</param>
+    /// <returns></returns>
+    public static CGrabToolSO GrabTool(string id)
+        => Address.Get<CGrabToolSO>(id);
+
+    /// <summary>
+    /// 문자열 ID로 산소 SO를 반환받습니다.
+    /// </summary>
+    /// <param name="id">산소 ID</param>
+    /// <returns></returns>
+    public static COxygenSO Oxygen(string id)
+        => Address.Get<COxygenSO>(id);
+
+    /// <summary>
+    /// 문자열 ID로 레이더 SO를 반환받습니다.
+    /// </summary>
+    /// <param name="id">레이더 ID</param>
+    /// <returns></returns>
+    public static CRadarSO Radar(string id)
+        => Address.Get<CRadarSO>(id);
+
+    /// <summary>
+    /// 문자열 ID로 추진기 SO를 반환받습니다.
+    /// </summary>
+    /// <param name="id">추진기 ID</param>
+    /// <returns></returns>
+    public static CThrusterSO Thruster(string id)
+        => Address.Get<CThrusterSO>(id);
+}
