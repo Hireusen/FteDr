a166fe2c1ee80795fc8a1ba94d471e40d1998a16
kimchaeho1998-prog <kimchaeho1998@gmail.com>
2026-06-27 23:59:20 +0900

    Chore: 전역 UI 매니저 설계

---
+++ Assets/Project/Scripts/Global/EventBus/UI/OnUIIOptionEvents.cs
@@ -0,0 +1,27 @@
+﻿/// <summary>
+/// 전역 설정(Options) 팝업창을 열어달라고 요청하는 이벤트 구조체입니다.
+/// </summary>
+public readonly struct OnRequestOpenSettings
+{
+    /// <summary>
+    /// 설정창 오픈 이벤트를 발행합니다.
+    /// </summary>
+    public static void Publish()
+    {
+        CEventBus<OnRequestOpenSettings>.Publish(new OnRequestOpenSettings());
+    }
+}
+
+/// <summary>
+/// 전역 설정(Options) 팝업창을 닫아달라고 요청하는 이벤트 구조체입니다.
+/// </summary>
+public readonly struct OnRequestCloseSettings
+{
+    /// <summary>
+    /// 설정창 닫기 이벤트를 발행합니다.
+    /// </summary>
+    public static void Publish()
+    {
+        CEventBus<OnRequestCloseSettings>.Publish(new OnRequestCloseSettings());
+    }
+}
