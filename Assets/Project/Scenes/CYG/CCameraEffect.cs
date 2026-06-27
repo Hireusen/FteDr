670beb599b76d74a85375c89750e9f684589a221
FteDr <cyg68700@naver.com>
2026-06-27 22:16:12 +0900

    feat: 하강제작(1/2)

---
+++ Assets/Project/Scenes/CYG/CCameraEffect.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// 클래스의 설계 의도입니다.
+/// </summary>
+public class CCameraEffect : AMono
+{
+    #region ─────────────────────────▶ 인스펙터 ◀─────────────────────────
+    //[Header("주제")]
+    //[SerializeField] private Class _class;
+    [SerializeField] Transform cam;
+    #endregion
+
+    #region ─────────────────────────▶ 내부 변수 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 공개 멤버 ◀─────────────────────────
+    public void CameraKick()
+    {
+        StartCoroutine(CameraKickCo());
+    }
+    #endregion
+
+    #region ─────────────────────────▶ 내부 메서드 ◀─────────────────────────
+    private IEnumerator CameraKickCo()
+    {
+        Vector3 origin = cam.localPosition;
+        Vector3 target = origin + Vector3.down * 0.05f;
+
+        float t = 0;
+
+        while (t < 0.03f)
+        {
+            cam.localPosition = Vector3.Lerp(origin, target, t / 0.03f);
+            t += Time.deltaTime;
+            yield return null;
+        }
+
+        t = 0;
+
+        while (t < 0.12f)
+        {
+            cam.localPosition = Vector3.Lerp(target, origin, t / 0.12f);
+            t += Time.deltaTime;
+            yield return null;
+        }
+
+        cam.localPosition = origin;
+    }
+    #endregion
+
+    #region ─────────────────────────▶ 메시지 함수 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 중첩 타입 ◀─────────────────────────
+
+    #endregion
+}
---
+++ Assets/Project/Scenes/CYG/CSubMarineUpDown.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// 클래스의 설계 의도입니다.
+/// </summary>
+public class CSubMarineUpDown : AMono
+{
+    #region ─────────────────────────▶ 인스펙터 ◀─────────────────────────
+    //[Header("주제")]
+    //[SerializeField] private Class _class;
+    #endregion
+
+    #region ─────────────────────────▶ 내부 변수 ◀─────────────────────────
+    bool _moveOn = false;
+    #endregion
+
+    #region ─────────────────────────▶ 공개 멤버 ◀─────────────────────────
+
+    //type=-1 하강, type=1 상승
+    public void MoveSubmarine(int type)
+    {
+        if (_moveOn == true) return;
+        _moveOn = true;
+        UFade.FadeOut(1.5f, true);
+        StartCoroutine(MoveSubmarineCo(type,3f));
+        
+    }
+    #endregion
+
+    #region ─────────────────────────▶ 내부 메서드 ◀─────────────────────────
+    private IEnumerator MoveSubmarineCo(int movetype,float duration)
+    {
+        float timer = 0f;
+        while (timer < duration)
+        {
+
+            switch (movetype)
+            {
+                case -1:
+                    gameObject.transform.position += Vector3.up * -10 * Time.deltaTime;
+                    break;
+                case 1:
+                    gameObject.transform.position += Vector3.up * +10 * Time.deltaTime;
+                    break;
+            }
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        _moveOn = false;
+        
+    }
+    #endregion
+
+    #region ─────────────────────────▶ 메시지 함수 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 중첩 타입 ◀─────────────────────────
+
+    #endregion
+}
