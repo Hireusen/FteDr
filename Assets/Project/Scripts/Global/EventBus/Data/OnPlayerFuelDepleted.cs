6a7f95e2579fafe59e050d66ceb4c1b3835dd5a0
JiRungYeeOwO <hosik9398@gmail.com>
2026-07-08 18:41:26 +0900

    chore : 연료 시스템 추가 구현

---
+++ Assets/Project/Scripts/Global/EventBus/Data/OnPlayerFuelDepleted.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// 연료가 0으로 고갈되는 순간 1회 발행합니다.
+/// </summary>
+public readonly struct OnPlayerFuelDepleted
+{
+    public static void Publish()
+    {
+        CEventBus<OnPlayerFuelDepleted>.Publish(new OnPlayerFuelDepleted());
+    }
+}
