80608c2d7c6346ad432667e2717a38a75662a2b9
FteDr <cyg68700@naver.com>
2026-07-11 23:42:42 +0900

    fix: 잡기 버그 fix
    이전 충돌객체관리가 덜교체되어서,
    현재 잡은 물체가 이전에 잡은 물체로 표기되는 현상이 있었음

--- Assets/Project/Scripts/Player/NewGrab/CFinger.cs
+++ Assets/Project/Scripts/Player/NewGrab/CFinger.cs
@@ -37,6 +37,14 @@ public class CFinger : AMono
     //objects -> crashobject 교체 시도
     public HashSet<GameObject>Objects  { get; private set; }=new HashSet<GameObject>();
     public HashSet<CrashInfo> CrashObjects { get;private set; }=new HashSet<CrashInfo>();
+
+    public void RemoveCrashinfo(CrashInfo crashInfo)
+    {
+        if (CrashObjects.Contains(crashInfo))
+        {
+            CrashObjects.Remove(crashInfo);
+        }
+    }
     #endregion
 
     #region ─────────────────────────▶ 내부 메서드 ◀─────────────────────────
@@ -90,7 +98,10 @@ public class CFinger : AMono
     private void OnTriggerEnter(Collider collision)
     {
         //objects에 넣은걸 교체하기 위한, crashobjects 코드 추가
-        if (collision.gameObject.CompareTag("Collectible"))
+        CrashInfo temp = new CrashInfo();
+        temp.crashedObject = collision.gameObject;
+        if (temp.crashedObject.CompareTag("Collectible"))
+        //if (collision.gameObject.CompareTag("Collectible"))
         {
             Objects.Add(collision.gameObject);
 
@@ -102,18 +113,21 @@ public class CFinger : AMono
 
             CrashObjects.Add(crashobject);
             print(_testnum + "attach");
+
         }
     }
     private void OnTriggerExit(Collider collision)
     {
-        if (collision.gameObject.CompareTag("Collectible"))
+        CrashInfo temp = new CrashInfo();
+        temp.crashedObject = collision.gameObject;
+        if (temp.crashedObject.CompareTag("Collectible"))
+        //if (collision.gameObject.CompareTag("Collectible"))
         {
             if (Objects.Contains(collision.gameObject))
             {
                 Objects.Remove(collision.gameObject);
                 print(_testnum + "deattach");
             }
-            CrashInfo temp = new CrashInfo();
             temp.crashedObject = collision.gameObject;
             if (CrashObjects.Contains(temp))
             {
