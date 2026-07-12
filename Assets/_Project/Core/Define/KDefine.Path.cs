38e4dc6b8655d77c5095de231b6c504a6abd9356
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-07-12 14:39:31 +0900

    asset : 프로젝트 폴더 구조 변경

---
+++ Assets/_Project/Core/Define/KDefine.Path.cs
@@ -0,0 +1,22 @@
+﻿/// <summary>
+/// 프로젝트의 폴더 및 파일 경로를 정의하는 매크로 클래스입니다.
+/// </summary>
+public static partial class K
+{
+    // 문자열 ID 폴더 경로
+    public const string STRING_ID_EXPORT_PATH = "Assets/_Project/Core/Define";
+
+    // 아이템 아이콘 스프라이트 경로
+    public const string ITEM_ICON_EXPORT_PATH = "Assets/_Project/ShareArt/Sprites";
+
+    // 오디오 에셋 폴더 경로
+    public const string SOUND_IMPORT_PATH = "Assets/_Project/Audios";
+    public const string SOUND_EXPORT_PATH = "Assets/Resources/ScriptableObjects/Sound";
+
+    // 리소스 폴더 경로
+    public const string RESOURCE_SO_PATH = "ScriptableObjects";
+
+    // 외부 대용량 에셋 경로
+    public const string ASSETS_IGNORED_FOLDER_NAME = "ThirdPartyIgnored";
+    public const string ASSETS_IGNORED_PATH = "Assets/ThirdPartyIgnored";
+}
