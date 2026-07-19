ba0cc9ac4890d3aee6963b4c605b041a580435e1
FteDr <cyg68700@naver.com>
2026-07-19 23:59:07 +0900

    chore: 플레이어 프리팹, 잠수함 프리팹 설정

--- Assets/_Project/Features/Camera/CFPPCamera.cs
+++ Assets/_Project/Features/Camera/CFPPCamera.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class CFPPCamera : AFrameable, ILateUpdateFrameable
 {
     #region ─────────────────────────▶ 인스펙터 ◀─────────────────────────
+    [SerializeField] private LayerMask _groundLayer;
     [Header("필수 연결")]
     [SerializeField] private CPlayerController _playerController;
     [Tooltip("컨트롤러가 회전을 넣는 눈높이 앵커. 평상시 카메라가 이 트랜스폼의 위치·회전을 복사")]
@@ -138,7 +139,15 @@ public class CFPPCamera : AFrameable, ILateUpdateFrameable
     private void NormalLookTick()
     {
         _yaw += _currentLookInput.x * Sensitivity;
-        _pitch -= _currentLookInput.y * Sensitivity;
+        float lookY = _currentLookInput.y;
+        Debug.Log(lookY);
+        
+        if (IsArmCrashedGround() && lookY<0)
+        {
+            lookY = 0;
+        }
+        
+        _pitch -= lookY * Sensitivity;
 
         bool swimming = _playerController.CurrentState == EPlayerState.Swimming;
         float min = swimming ? _swimPitchMin : _groundPitchMin;
@@ -229,6 +238,15 @@ public class CFPPCamera : AFrameable, ILateUpdateFrameable
     {
         if (euler > 180f) euler -= 360f;
         return euler;
+    }
+
+    private bool IsArmCrashedGround()
+    {
+        bool result = true;
+        int mask = ~LayerMask.GetMask("Player");
+        result = Physics.CheckSphere(_cameraRoot.transform.position, 0.1f,mask);
+
+        return false;
     }
     #endregion
 
