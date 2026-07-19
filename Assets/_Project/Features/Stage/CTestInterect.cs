3ba2da9aba6eca4e4365766c28b824a88ef8ddcf
FteDr <cyg68700@naver.com>
2026-07-19 16:44:20 +0900

    feat: 상호작용 관련 테스트

---
+++ Assets/_Project/Features/Stage/CTestInterect.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 클래스의 설계 의도입니다.
+/// </summary>
+public class CTestInterect : AMono,IInterect
+{
+    #region ─────────────────────────▶ 인스펙터 ◀─────────────────────────
+    //[Header("주제")]
+    //[SerializeField] private Class _class;
+    #endregion
+
+    #region ─────────────────────────▶ 내부 변수 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 공개 멤버 ◀─────────────────────────
+    public void Interect()
+    {
+        print("상호작용 테스트");
+    }
+
+    #endregion
+
+    #region ─────────────────────────▶ 내부 메서드 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 메시지 함수 ◀─────────────────────────
+
+    #endregion
+
+    #region ─────────────────────────▶ 중첩 타입 ◀─────────────────────────
+
+    #endregion
+}
