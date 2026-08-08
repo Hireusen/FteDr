8553ff0aad07cabbfa348adce29d0b83dce2411c
FteDr <cyg68700@naver.com>
2026-08-08 16:31:34 +0900

    feat: 조준단계 추가(프로토타입)

--- Assets/_Project/Features/Collectible/Prefab/Grab/CDiverToAim.cs
+++ Assets/_Project/Features/Collectible/Prefab/Grab/CDiverToAim.cs
@@ -6,9 +6,11 @@ public class CDiverToAim : MonoBehaviour
     [SerializeField] private CNewGrab _newGrab;
     [SerializeField] private Transform _arm;
     [SerializeField] private Transform _playerCam;
-
-    public (CNewGrab newGrab, Transform arm, Transform playerCam) GetReference()
+    public GameObject AimCanvas { get;private set; }
+    public (CNewGrab newGrab, Transform arm, Transform playerCam) GetReference(GameObject aimCanvas)
     {
+        AimCanvas = aimCanvas;
         return (_newGrab, _arm, _playerCam);
     }
+
 }
