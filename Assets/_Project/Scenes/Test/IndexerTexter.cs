8ce3cfc2aff2b36257431822e1e866a30c0b0135
Hireusen <108818326+Hireusen@users.noreply.github.com>
2026-09-19 16:28:50 +0900

    feat : 모든 유니크 수집품 콜라이더 작업

---
+++ Assets/_Project/Scenes/Test/IndexerTexter.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 클래스의 설계 의도입니다.
+/// </summary>
+public class IndexerTexter : MonoBehaviour
+{
+    public class Inventory
+    {
+        private int[] _items;
+
+        public Inventory(int length)
+        {
+            _items = new int[length];
+            for (int i = 0; i < _items.Length; i++)
+            {
+                _items[i] = i * 10; // 예시로 초기화
+            }
+        }
+
+        public int Length => _items.Length;
+
+        public int this[int index]
+        {
+            get
+            {
+                return _items[index];
+            }
+            set
+            {
+                _items[index] = value;
+            }
+        }
+    }
+
+    private Inventory _inventory;
+    private void Awake()
+    {
+        _inventory = new Inventory(1000);
+    }
+
+    private int _cursor = 0;
+    private void Update()
+    {
+        _cursor++;
+        _cursor %= _inventory.Length;
+        int item = _inventory[_cursor];
+        UDebug.Print($"인벤토리에서 {_cursor}번째 아이템: {_inventory[_cursor]}");
+    }
+}
